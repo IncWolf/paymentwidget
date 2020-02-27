889f1da09746d1a8f7dc37931a931a7328f173e9
IncWolf <incwolf@gmail.com>
2020-02-27 22:55:06 +0200

    Updates

---
+++ precache-manifest.f559598b211e6770ab50ae6c5c019d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dba78f59b42bbcd312132ee2d701619",
+    "url": "/paymentwidget/index.html"
+  },
+  {
+    "revision": "d34b7503318662248f34",
+    "url": "/paymentwidget/static/js/2.d4a562b3.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/paymentwidget/static/js/2.d4a562b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5f9c7c90680bd340e6",
+    "url": "/paymentwidget/static/js/main.b27e53d5.chunk.js"
+  },
+  {
+    "revision": "4036bf240d6b8abac1b1",
+    "url": "/paymentwidget/static/js/runtime-main.4e9eb98e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/paymentwidget/precache-manifest.f559598b211e6770ab50ae6c5c019d2d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paymentwidget/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
